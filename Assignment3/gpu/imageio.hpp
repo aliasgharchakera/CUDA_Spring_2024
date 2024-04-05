749294b2b3c8035c1e322df6f955be0029fe3317
Ali Asghar <aliasghar.chakera@gmail.com>
2024-04-05 22:17:36 +0500

    updated imageio

--- Assignment3/gpu/imageio.hpp
+++ Assignment3/gpu/imageio.hpp
@@ -8,38 +8,28 @@
 #include "vector.hpp"
 
 #pragma endregion
+#include <fstream>
 
-//-----------------------------------------------------------------------------
-// System Includes
-//-----------------------------------------------------------------------------
-#pragma region
-
-#include <cstdio>
-
-#pragma endregion
-
-//-----------------------------------------------------------------------------
-// Declarations and Definitions
-//-----------------------------------------------------------------------------
 namespace smallpt {
 
-	inline void WritePPM(std::uint32_t w, 
-						 std::uint32_t h, 
-						 const Vector3* Ls, 
-						 const char* fname = "cu-image.ppm") noexcept {
-		
-		FILE* fp;
-		
-		fopen_s(&fp, fname, "w");
-		
-		std::fprintf(fp, "P3\n%u %u\n%u\n", w, h, 255u);
-		for (std::size_t i = 0; i < w * h; ++i) {
-			std::fprintf(fp, "%u %u %u ", 
-						 ToByte(Ls[i].m_x), 
-						 ToByte(Ls[i].m_y), 
-						 ToByte(Ls[i].m_z));
+	inline void WritePPM(std::uint32_t w,
+						 std::uint32_t h,
+						 const Vector3* Ls,
+						 const char* fname = "gpu-image.ppm") noexcept {
+
+		std::ofstream file(fname);
+		if (!file.is_open()) {
+			// Handle file open error
+			return;
 		}
-		
-		std::fclose(fp);
+
+		file << "P3\n" << w << " " << h << "\n255\n";
+		for (std::size_t i = 0; i < w * h; ++i) {
+			file << static_cast<int>(ToByte(Ls[i].m_x)) << " "
+				 << static_cast<int>(ToByte(Ls[i].m_y)) << " "
+				 << static_cast<int>(ToByte(Ls[i].m_z)) << " ";
+		}
+
+		file.close();
 	}
 }
